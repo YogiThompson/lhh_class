e32208aa8904ec39fd265f57c05c46a69192d83c
Yogi Thompson <yogi314159@hotmail.com>
2018-10-07 13:31:51 -0700

    Implement shuffle and deal one row of cards face-up.

--- deal.js
+++ deal.js
@@ -1,5 +1,10 @@
 const columnCount = 7;
-
 function deal() {
-    alert("Deal!");
+    for (let i=0; i<7; i++) {
+        
+   let card= this.cards[i];
+   card.reversed=false 
+   this.columns[i].cards.push(card);
+    }
 }
+
