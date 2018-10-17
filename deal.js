504a696cc2d49ca476b334e86819debf76e26dae
Yogi Thompson <yogi314159@hotmail.com>
2018-10-17 10:00:40 -0700

    Lay out the cards in the right arrangement.

--- deal.js
+++ deal.js
@@ -1,10 +1,63 @@
 const columnCount = 7;
 function deal() {
-    for (let i=0; i<7; i++) {
-        
-   let card= this.cards[i];
-   card.reversed=false 
-   this.columns[i].cards.push(card);
-    }
-}
 
+
+
+
+    for (let r = 0; r < 7; r++) {
+        for (let i = r; i < 7; i++){
+
+            let card = this.cards[i];
+            card.reversed = true
+            this.columns[i].cards.push(card);
+        }
+    }
+
+
+
+
+/*
+    for (let i = 1; i < 7; i++) {
+
+        let card = this.cards[i];
+        card.reversed = true
+        this.columns[i].cards.push(card);
+
+
+    }
+    for (let i = 2; i < 7; i++) {
+
+        let card = this.cards[i];
+        card.reversed = true
+        this.columns[i].cards.push(card);
+    }
+
+    for (let i = 3; i < 7; i++) {
+
+        let card = this.cards[i];
+        card.reversed = true
+        this.columns[i].cards.push(card);
+    }
+
+    for (let i = 4; i < 7; i++) {
+
+        let card = this.cards[i];
+        card.reversed = true
+        this.columns[i].cards.push(card);
+    }
+
+    for (let i = 5; i < 7; i++) {
+
+        let card = this.cards[i];
+        card.reversed = true
+        this.columns[i].cards.push(card);
+    }
+
+
+    for (let i = 6; i < 7; i++) {
+
+        let card = this.cards[i];
+        card.reversed = true
+        this.columns[i].cards.push(card);
+    }*/
+}
